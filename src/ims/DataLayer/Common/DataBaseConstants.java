50d8cb12427f592ff281c770d346c22660a0e4ca
Jeanyhwh Desulme <jdesulme@gmail.com>
2012-05-09 19:44:08 -0400

    adding the correct mysql db connection

--- src/ims/DataLayer/Common/DataBaseConstants.java
+++ src/ims/DataLayer/Common/DataBaseConstants.java
@@ -14,11 +14,9 @@ public  class  DataBaseConstants {
     public static final String ODBC_USERNAME    = "admin";
     public static final String ODBC_PASSWORD    = "";
     
-    
-    public static final String MYSQL_DRIVER     = "sun.jdbc.odbc.JdbcOdbcDriver";
+    public static final String MYSQL_DRIVER     = "com.mysql.jdbc.Driver";
     public static final String MYSQL_URL        = "jdbc:mysql://localhost/warehouse";
     public static final String MYSQL_USERNAME   = "root";
-    public static final String MYSQL_PASSWORD   = "";
-   
+    public static final String MYSQL_PASSWORD   = "";  
 }
 
